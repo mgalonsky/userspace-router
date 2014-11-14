f71dad04b55f368de5613f24809c547ada36161a
HW_Seax <helen@Seax.(none)>
2014-11-14 15:51:45 -0800

    Made first version, it prints the eth and ip headers of the first 5 packets, takes the iface as a cmd line argument

--- userspace_router.cpp
+++ userspace_router.cpp
@@ -3,10 +3,36 @@
  * A simple userspace router meant for HMC CS125 lab 5
  */
 
+
+#include <iostream>
+#include <string>
+#include <crafter.h>
+//will want to add pthread too
+
+using namespace std;
+using namespace Crafter;
+
+
 //Your per-packet router code goes here
-void pkt_callback(Packet& pkt){
-    print "Received an Ethernet packet. MAC src:", pkt.src, "MAC dst:",pkt.dst
-    print pkt.summary()
+void packetHandler(Packet* packet, void* user){
+
+	Ethernet* ethHeader = packet->GetLayer<Ethernet>();
+	IP* ipHeader = packet->GetLayer<IP>();
+
+	if (ethHeader == nullptr) {
+		cout << "ethHeader was null" << endl;
+	}
+	else {
+		ethHeader->Print();
+	}
+	if (ipHeader == nullptr) {
+		cout << "ipHeader was null" << endl;
+	}
+	else {
+		ipHeader->Print();
+	}
+
+	cout<<"--------------------------------------"<<endl;
 
     //Determine if it is an IP packet. If not then return
 
@@ -19,7 +45,7 @@ void pkt_callback(Packet& pkt){
     //Find the next hop (gateway) for the destination *network* and look up the MAC address of that router
 
     //Determine the outgoing interface and MAC address needed to reach the next-hop router
-    OUT_IFACE="eth4"
+//    OUT_IFACE="eth4"
 
     //Modify the SRC and DST MAC addresses to match the outgoing interface and the DST MAC found above
 
@@ -27,15 +53,21 @@ void pkt_callback(Packet& pkt){
 
     //Send the packet out the proper interface as required to reach the next hop router. Use:
 
-    sendp(pkt, iface=OUT_IFACE)
+	//I think this call becomes pkt.send(iface)
+//    sendp(pkt, iface=OUT_IFACE)
 }
 
 //Main code here...
 int main(int argc, char* argv[]){
 
+	string iface = argv[1];
+	Sniffer sniff("", iface, packetHandler);
+	sniff.Capture(5);
     //First setup your routing table either as global variables or as objects passed to pkt_callback
     //And any other init code
 
-    //Start the packet sniffer
-    sniff(prn=pkt_callback, store=0)
+	//set up a sniffer for all interfaces
+	//if possible get 1 that uses all interfaces and call caputre on it with -1
+	//otherwise will have to create a seperate sniffer for each interface and spawn each one before blocking to avoid finishing
+
 }
